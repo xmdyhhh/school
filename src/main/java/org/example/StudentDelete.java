401897d36ebbec7051127f0575dce0364e8266b1
FutureZWJ <2626629983@qq.com>
2026-05-11 17:03:49 +0800

    张文静

---
+++ src/main/java/org/example/StudentDelete.java
@@ -0,0 +1,22 @@
+package org.example;
+import java.util.Scanner;
+public class StudentDelete {
+    public class StudentUpdate {
+        public static void updateStudent() {
+            System.out.print("请输入要修改的学生学号：");
+            String id = sc.next();
+            for (Student s : studentList) {
+                if (s.getId().equals(id)) {
+                    System.out.print("新姓名：");
+                    s.setName(sc.next());
+                    System.out.print("新年龄：");
+                    s.setAge(sc.nextInt());
+                    System.out.print("新性别：");
+                    s.setGender(sc.next());
+                    System.out.println("修改成功！");
+                    return;
+                }
+            }
+            System.out.println("学号不存在！");
+        }
+}
